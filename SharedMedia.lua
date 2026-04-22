47e5a000d0c3f6695877ee861f22d60be33304e5
Alex <alex@cornellier.com>
2026-04-22 19:28:03 -0400

    bring back Circle.tga

--- SharedMedia.lua
+++ SharedMedia.lua
@@ -3,7 +3,7 @@ local MediaType_BORDER = LSM.MediaType.BORDER or "border"
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR or "statusbar"
 
 -- statusbars:
-LSM:Register(MediaType_STATUSBAR, "Ort_Spiritfont", [[Interface\AddOns\OrtemisTools\statusbar\Ort_Spiritfont.tga]])
+LSM:Register(MediaType_STATUSBAR, "Circle", [[Interface\AddOns\OrtemisTools\statusbar\Circle.tga]])
 
 -- borders
 LSM:Register(MediaType_BORDER, "Ferous 13", [[Interface\AddOns\OrtemisTools\border\Ferous13.tga]])
